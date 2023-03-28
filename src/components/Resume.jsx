01598ad8d264ed87cee870fbd7d8e4752bdf3d1c
Sam O'Cain <samocain93@gmail.com>
2023-03-28 16:26:52 -0500

    added pdf for resume instead of drive link

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,6 +3,7 @@ import { AcademicCapIcon } from '@heroicons/react/solid';
 import { Document, Page } from 'react-pdf';
 import resume1 from '../assets/images/resume1.png';
 import resume2 from '../assets/images/resume2.png';
+import resume from '../assets/pdf/resume.pdf'
 
 export default function Resume() {
   return (
@@ -10,7 +11,8 @@ export default function Resume() {
       <div className='container px-5 py-6 mx-auto text-center lg-px-40'>
         <div className='flex flex-col mb-20'>
           <AcademicCapIcon className='mx-auto inline-block w-10 mb-4' />
-          <a href="https://docs.google.com/document/d/1rTCYJWahFMkvd4iSxxIYdGw0Q3Q_lmC7p45AIldxz94/edit?usp=sharing" target="_blank">
+          {/* <a href="https://docs.google.com/document/d/1rTCYJWahFMkvd4iSxxIYdGw0Q3Q_lmC7p45AIldxz94/edit?usp=sharing" target="_blank"> */}
+          <a href={resume} target="_blank">
           <h1 className='sm:text-4xl text-3xl font-medium  title-font mb-4 text-white hover:text-green-400'>
             Resume
           </h1>
