4dc635bb1c2d24ac68c4fea2e1ccc777da41b9b7
Sam O'Cain <samocain93@gmail.com>
2023-03-29 21:15:47 -0500

    added footer element and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { BsLinkedin, BsGithub, BsLink, BsGit } from 'react-icons/bs';
 
 export default function Footer() {
   return (
-    <div className="bg-gray-900">Footer</div>
-  )
+    <footer className='footer bg-gray-900'>
+      <div className='footer__links container flex flex-wrap flex-row p-5 items-center justify-center mx-auto md:justify-evenly'>
+        <a href='#about'>
+          <p className='footer__text text-xl hover:text-white mr-3'>
+            Sam O'Cain
+          </p>
+        </a>
+
+        <a
+          href='https://www.linkedin.com/in/samocain/'
+          className='ml-3 mr-3 text-2xl hover:text-white'
+          target='_blank'
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href='https://github.com/samocain93'
+          target='_blank'
+          className='ml-3 mr-3 text-2xl hover:text-white'
+        >
+          <BsGithub />
+        </a>
+      </div>
+    </footer>
+  );
 }
